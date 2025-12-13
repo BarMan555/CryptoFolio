8befb3c919375599fdb78ebdc3800cebd776f342
Boris Shesterikov <boris20033@gmail.com>
2025-12-13 22:22:20 +0400

    Rename ContentView to HomeView and add true ContentView

--- CryptoFolio/Views/ContentView.swift
+++ CryptoFolio/Views/ContentView.swift
@@ -2,42 +2,14 @@
 //  ContentView.swift
 //  CryptoFolio
 //
-//  Created by Борис Шестериков on 07.12.2025.
+//  Created by Борис Шестериков on 13.12.2025.
 //
 
 import SwiftUI
-import SwiftData
 
 struct ContentView: View {
-    @State private var coinVM = CoinViewModel()
-    @State private var searchText = ""
-    var filteredCoins: [Coin] {
-        // 1. Если поиск пустой — возвращаем весь список
-        if searchText.isEmpty {
-            return coinVM.coins
-        }
-        
-        // 2. Иначе фильтруем: ищем совпадение в имени
-        return coinVM.coins.filter({coin in
-            coin.name.localizedCaseInsensitiveContains(searchText)
-        })
-    }
     var body: some View {
-        NavigationStack{
-            List(filteredCoins){ coin in
-                HStack{
-                    NavigationLink(destination: DetailView(coin: coin)) {
-                        CoinRow(coin: coin)
-                    }
-                }
-            }
-            .navigationTitle("Crypto Market")
-            .onAppear(perform: coinVM.fetchCoins)
-            .searchable(text: $searchText, prompt: "Найти монетy")
-            .refreshable{
-                coinVM.fetchCoins()
-            }
-        }
+        HomeView()
     }
 }
 
